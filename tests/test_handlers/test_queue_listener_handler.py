37f185dc76cd4e38aad002a068b7c817b6edba2d
Zobayer Hasan <zobayer1@gmail.com>
2021-09-23 18:47:23 +0600

    Add expand-user support through environment variables

--- tests/test_handlers/test_queue_listener_handler.py
+++ tests/test_handlers/test_queue_listener_handler.py
@@ -5,38 +5,38 @@ import pytest
 import yaml
 
 config_yaml = """
-    version: 1
-    objects:
-      queue:
-        class: queue.Queue
-        maxsize: 1000
-    formatters:
-      simple:
-        format: '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+version: 1
+objects:
+  queue:
+    class: queue.Queue
+    maxsize: 1000
+formatters:
+  simple:
+    format: '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+handlers:
+  console:
+    class: logging.StreamHandler
+    formatter: simple
+    stream: ext://sys.stdout
+  queue_handler:
+    class: logging_.handlers.QueueListenerHandler
     handlers:
-      console:
-        class: logging.StreamHandler
-        formatter: simple
-        stream: ext://sys.stdout
-      queue_handler:
-        class: logging_.handlers.QueueListenerHandler
-        handlers:
-          - cfg://handlers.console
-        queue: cfg://objects.queue
-    loggers:
-      test_logger:
-        level: DEBUG
-        handlers:
-          - queue_handler
-        propagate: yes
-    root:
-      level: NOTSET
-      handlers:
-        - console
+      - cfg://handlers.console
+    queue: cfg://objects.queue
+loggers:
+  test_logger:
+    level: DEBUG
+    handlers:
+      - queue_handler
+    propagate: yes
+root:
+  level: NOTSET
+  handlers:
+    - console
 """
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def logger():
     """Fixture for providing a configured logger object"""
     logging_config = yaml.safe_load(config_yaml)
