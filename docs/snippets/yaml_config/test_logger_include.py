b811925a54f2147d5c345bfaffaafdd37c7cad8b
Adam Greene <github@mzpqnxow.com>
2022-01-29 13:21:24 -0500

    add test config and driver for !include

---
+++ docs/snippets/yaml_config/test_logger_include.py
@@ -0,0 +1,16 @@
+import logging
+from logging_.config import YAMLConfig
+
+with open("logging-include.yaml", "r") as config_file:
+    YAMLConfig(config_file.read(), silent=True)
+
+# alternatively, you can use
+# YAMLConfig.from_file("logging.yaml", silent=True)
+
+logger = logging.getLogger("test_logger")
+
+logger.debug("This is a debug log")
+logger.info("This is an info log")
+logger.warning("This is an warning log")
+logger.error("This is an error log")
+logger.critical("This is a critical log")
